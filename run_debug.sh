99b1317cdd2e01e1de13bab517ef9045ce692f4a
Ellis Percival <flyte@failcode.co.uk>
2020-05-23 17:49:15 +0100

    Change port used in test. Cleanup gpio.

---
+++ run_debug.sh
@@ -0,0 +1,1 @@
+python -m ptvsd --host 0.0.0.0 --port 3500 --wait -m pi_mqtt_gpio.server config.yml
--- tests/pi/test_raspberrypi.py
+++ tests/pi/test_raspberrypi.py
@@ -5,7 +5,7 @@ from pi_mqtt_gpio.server import install_missing_requirements
 
 
 # There is a physical jumper between these two pins on the test Pi
-JUMPER_PINS = (17, 27)
+JUMPER_PINS = (22, 27)
 
 
 def test_digital_io():
@@ -13,14 +13,13 @@ def test_digital_io():
     Uses the module to set digital output and read it with a digital input.
     """
     gpio = raspberrypi.GPIO({})
-    pin_in = JUMPER_PINS[0]
-    pin_out = JUMPER_PINS[1]
+    pin_in, pin_out = JUMPER_PINS
 
     gpio.setup_pin(pin_in, PinDirection.INPUT, PinPullup.DOWN, {})
     gpio.setup_pin(pin_out, PinDirection.OUTPUT, None, {})
 
-    assert gpio.get_pin(pin_in) is False
     gpio.set_pin(pin_out, True)
     assert gpio.get_pin(pin_in) is True
     gpio.set_pin(pin_out, False)
     assert gpio.get_pin(pin_in) is False
+    gpio.cleanup()
